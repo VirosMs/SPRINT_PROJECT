955a8695074f147f7baca119941419f4977e4c3c
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2024-02-14 14:15:18 +0100

    Add appbar login_Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:sprint/widget/appbar_widget.dart';
+
 
 class LoginScreen extends StatelessWidget{
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      appBar: AppBarWidget(
+          title: 'Login'
+        ),
+      
+      body: Center(
+        child: Text('Login Form'), // Change to reflect register content
+      ),
+    );
   }
 
 }
